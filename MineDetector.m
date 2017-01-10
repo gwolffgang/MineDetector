d9bff06636fcaa73e5a1de0481b837cea5e836f7
Georg Wolffgang <gwolffgang@fh-bielefeld.de>
2017-01-10 18:16:39 +0100

    Größeres Update

--- MineDetector.m
+++ MineDetector.m
@@ -1,167 +1,116 @@
 function MineDetector( action )
-%UNTITLED5 Summary of this function goes here
+%MINEDETECTOR Summary of this function goes here
 %   Detailed explanation goes here
-MD=mfilename;
 
+MD = mfilename;
+global values;
 
-global vh width height;
-if nargin==0
-                   
-    
-vh.fig_handle=figure('NumberTitle','off',...
-                  'Name','MineDetector');
-set(vh.fig_handle,'Menubar','none')              
-vh.pb_handle1=uicontrol   (vh.fig_handle,'Style','push',...
-                                         'Unit','normalized',...
-                                         'position',[0.25 0.7 0.5 0.3],...
-                                         'String','Easy',...
-                                         'Callback',[MD ' Easy']);
-vh.pb_handle2=uicontrol   (vh.fig_handle,'Style','push',...
-                                         'Unit','normalized',...
-                                         'position',[0.25 0.35 0.5 0.3],...
-                                         'String','Medium',...
-                                         'Callback',[MD ' Medium']);
-vh.pb_handle3=uicontrol   (vh.fig_handle,'Style','push',...
-                                         'Unit','normalized',...
-                                         'position',[0.25 0 0.5 0.3],...
-                                         'String','Hard',...
-                                         'Callback',[MD ' Hard']);  
-
+if nargin == 0
+    values.fig_handle = figure('NumberTitle','off',...
+        'Name','MineDetector');
+    set(values.fig_handle,'Menubar','none');
+    values.pb_handle_easy = uicontrol(values.fig_handle,'Style','push',...
+        'Unit','normalized',...
+        'Position',[0.25 0.7 0.5 0.3],...
+        'String','Beginner',...
+        'Callback',[MD ' Beginner']);
+    values.pb_handle_medium = uicontrol(values.fig_handle,'Style','push',...
+        'Unit','normalized',...
+        'Position',[0.25 0.35 0.5 0.3],...
+        'String','Intermediate',...
+        'Callback',[MD ' Intermediate']);
+    values.pb_handle_hard = uicontrol(values.fig_handle,'Style','push',...
+        'Unit','normalized',...
+        'position',[0.25 0 0.5 0.3],...
+        'String','Expert',...
+        'Callback',[MD ' Expert']);  
 else 
     switch action
-        case  'Easy'
-         delete (vh.pb_handle1);
-         delete (vh.pb_handle2); 
-         delete (vh.pb_handle3);   
-         height=9;
-         width=9;
-         mines=4;
-         vh.Minefield=zeros(height,width);
-         P=randperm(width*height);                                     
-         for i=1:1:height
-            for j=1:1:width
-                if P((i-1)*width+j)<=mines
-                    vh.Minefield(i,j)=-1;
-                end
-            end
-         end
-         for i=1:1:height
-               for j=1:1:width
-                   a=0;
-                   if vh.Minefield(i,j)~=-1
-                       for k=i-1:1:i+1
-                           for l=j-1:1:j+1
-                               if (l>0 && l<=width && k>0 && k<=height)
-                                   if vh.Minefield(k,l)==-1
-                                       a=a+1;
-                                   end
-                               end
-                           end
-                       end 
-                       vh.Minefield(i,j)=a;
-                   end
-               end
-         end
-         case  'Medium'
-         delete (vh.pb_handle1);
-         delete (vh.pb_handle2); 
-         delete (vh.pb_handle3);   
-         height=16;
-         width=16;
-         mines=40;
-         vh.Minefield=zeros(height,width);
-         P=randperm(width*height);                                     
-         for i=1:1:height
-            for j=1:1:width
-                if P((i-1)*width+j)<=mines
-                    vh.Minefield(i,j)=-1;
-                end
-            end
-         end
-         for i=1:1:height
-               for j=1:1:width
-                   a=0;
-                   if vh.Minefield(i,j)~=-1
-                       for k=i-1:1:i+1
-                           for l=j-1:1:j+1
-                               if (l>0 && l<=width && k>0 && k<=height)
-                                   if vh.Minefield(k,l)==-1
-                                       a=a+1;
-                                   end
-                               end
-                           end
-                       end 
-                       vh.Minefield(i,j)=a;
-                   end
-               end
-         end
-         case  'Hard'
-         delete (vh.pb_handle1);
-         delete (vh.pb_handle2); 
-         delete (vh.pb_handle3);   
-         height=16;
-         width=30;
-         mines=99;
-         vh.Minefield=zeros(height,width);
-         P=randperm(width*height);                                     
-         for i=1:1:height
-            for j=1:1:width
-                if P((i-1)*width+j)<=mines
-                    vh.Minefield(i,j)=-1;
-                end
-            end
-         end
-         for i=1:1:height
-               for j=1:1:width
-                   a=0;
-                   if vh.Minefield(i,j)~=-1
-                       for k=i-1:1:i+1
-                           for l=j-1:1:j+1
-                               if (l>0 && l<=width && k>0 && k<=height)
-                                   if vh.Minefield(k,l)==-1
-                                       a=a+1;
-                                   end
-                               end
-                           end
-                       end 
-                       vh.Minefield(i,j)=a;
-                   end
-               end
-         end
+        case 'Beginner'
+            values.height = 9;
+            values.width = 9;
+            values.mines = 10;
+            new_game;
+        case 'Intermediate'
+            values.height = 16;
+            values.width = 16;
+            values.mines = 40;
+            new_game;
+        case 'Expert'
+            values.height = 16;
+            values.width = 30;
+            values.mines = 99;
+            new_game;
     end
-    parts =strsplit(action,'|');
-    if strcmp(parts(1),'cb') == 1
-    x=str2double(parts(2));
-    y=str2double(parts(3));
-    if vh.Minefield(x,y)==0
-       deletebuttons(vh,x,y,width,height);
-    else
-        if vh.Minefield(x,y)==(-1)
-            close(vh.fig_handle);
+    
+    parts = strsplit(action,'|');
+    if strcmp(parts(1),'lc') == 1 || strcmp(parts(1),'rc') == 1
+        x = str2double(parts(2));
+        y = str2double(parts(3));
+        if strcmp(parts(1),'lc') == 1
+            if strcmp(get(values.field(x,y),'string'),'Mine?!') == 0
+                if values.Minefield(x,y) == 0
+                    deletebuttons(x, y);
+                else
+                    if values.Minefield(x,y) == -1
+                        close(values.fig_handle);
+                    else
+                        delete(values.field(x,y));
+                    end
+                end
+            end
         else
-             delete(vh.field(x,y));
+            if strcmp(get(values.field(x,y),'string'),'') == 1
+                set(values.field(x,y),'string','Mine?!');
+            else
+                set(values.field(x,y),'string','');
+            end
         end
-    end
+
     else
-       field=[1/width 0.9/height]; 
-       for i=1:1:height
-        for j=1:1:width
-        t1=num2str(i);
-        t2=num2str(j);
-        t3=num2str(vh.Minefield(i,j));
-        cb=strcat(' cb','|', t1,'|',t2);
-        vh.cb_txt=uicontrol(vh.fig_handle,'Style','text',...
-        'Unit','normalized',...
-        'position',[1/width*(j-1) 0.9/height*(i-1) field],...
-        'String',t3); 
-        vh.handlename=uicontrol(vh.fig_handle,...
-        'Style','push',...
-        'Unit','normalized',...
-        'position',[1/width*(j-1) 0.9/height*(i-1) field],...
-        'Callback',[MD cb]);
-        vh.field(i,j)=vh.handlename;
+        field_size = [1/values.width 0.9/values.height]; 
+        for i=1:1:values.height
+            for j=1:1:values.width
+                field_x = num2str(i);
+                field_y = num2str(j);
+                field_code = num2str(values.Minefield(i,j));
+                if field_code == '0'
+                    field_code = '';
+                end
+                color = 'black';
+                switch values.Minefield(i,j)
+                    case 1
+                        color = 'blue';
+                    case 2
+                        color = '[.2 .4 .0]';
+                    case 3
+                        color = 'red';
+                    case 4
+                        color = '[.5 .0 .5]';
+                    case 5
+                        color = '[.54 .27 .07]';
+                    case 6
+                        color = 'cyan';
+                end
+                cb_rightclick = strcat(' rc|', field_x, '|', field_y);
+                cb_leftclick = strcat(' lc|', field_x, '|', field_y);
+                values.cb_txt = uicontrol(values.fig_handle,...
+                    'Style','edit',...
+                    'Unit','normalized',...
+                    'Backgroundcolor','[.95 .95 .95]',...
+                    'Position',[1/values.width*(j-1) 0.9/values.height*(i-1) field_size],...
+                    'String',field_code,...
+                    'Enable','inactive',...
+                    'FontWeight','bold',...
+                    'FontSize',10,...
+                    'Foregroundcolor',color);
+                values.field(i,j) = uicontrol(values.fig_handle,...
+                    'Style','push',...
+                    'Unit','normalized',...
+                    'Position',[1/values.width*(j-1) 0.9/values.height*(i-1) field_size],...
+                    'ButtonDownFcn',[MD cb_rightclick],...
+                    'Callback',[MD cb_leftclick]);
+            end
         end
-       end
     end
 end
-
--- deletebuttons.m
+++ deletebuttons.m
@@ -1,27 +1,29 @@
-function  deletebuttons(vh,x,y,width,height)
-%UNTITLED Summary of this function goes here
+function deletebuttons(x,y)
+%DELETEBUTTONS Summary of this function goes here
 %   Detailed explanation goes here
+global values;
+
 M=[x
    y];
-delete(vh.field(x,y));
+delete(values.field(x,y));
 while(~isempty(M))
-      for i=(-1):1:1
-          for j=(-1):1:1
-              if ((M(1,1)+i) > 0 && (M(2,1)+j) > 0 &&(M(1,1)+i) < width  && (M(2,1)+j) < height )
-                    ishandle(vh.field(M(1,1)+i,M(2,1)+j))
-                  if (ishandle(vh.field(M(1,1)+i,M(2,1)+j))) 
-                    delete(vh.field(M(1,1)+i,(M(2,1)+j)))
-                     if (vh.Minefield(M(1,1)+i,(M(2,1)+j))==0)
-                         A=[x+i 
-                            y+j];
-                        M=[M A]
-                     end
-                  end
-              end
-          end
-      end
-      M(:,1)=[];
+    field_x = M(1,1);
+    field_y = M(2,1);
+    for i=(-1):1:1
+        for j=(-1):1:1
+            if field_x+i > 0 && field_y+j > 0 && field_x+i <= values.height && field_y+j <= values.width
+                if ~isempty(values.field(field_x+i, field_y+j )) && ishandle(values.field(field_x+i, field_y+j ))
+                    delete(values.field(field_x+i,(field_y+j)))
+                    if (values.Minefield(field_x+i,(field_y+j)) == 0)
+                        A=[field_x+i 
+                           field_y+j];
+                        M=[M A];
+                    end
+                end
+            end
+        end
+    end
+    M(:,1)=[];
 end
 
 end
-
